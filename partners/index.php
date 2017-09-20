6ea782a3e0a338dfbf139ba06ded5e5e3ba9208b
Ilya Krasnitsky <burrmanapps@gmail.com>
2017-09-20 12:27:59 +0300

    ex1-12

--- partners/index.php
+++ partners/index.php
@@ -1,4 +1,7 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Условия сотрудничества");
-?>Text here....<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?>text...
+<div class="article-text-block">
+</div>
+ <br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
