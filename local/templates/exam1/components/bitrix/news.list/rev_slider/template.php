9a2c5e423779086fd195dd0d17140c1e3213eac8
Ilya Krasnitsky <burrmanapps@gmail.com>
2017-09-20 16:08:02 +0300

    ex1-15

--- local/templates/exam1/components/bitrix/news.list/rev_slider/template.php
+++ local/templates/exam1/components/bitrix/news.list/rev_slider/template.php
@@ -15,28 +15,28 @@ $this->setFrameMode(true);
 
 <div class="item-wrap">
      <div class="rew-footer-carousel">
-     <?foreach($arResult["ITEMS"] as $arItem):?>
-		<div class="item">
-			<div class="side-block side-opin">
-				<div class="inner-block">
-					<div class="title">
-						<div class="photo-block">
-							<img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" alt="photo">
+	     <?foreach($arResult["ITEMS"] as $arItem):?>
+			<div class="item">
+				<div class="side-block side-opin">
+					<div class="inner-block">
+						<div class="title">
+							<div class="photo-block">
+								<img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" alt="photo">
+							</div>
+							<div class="name-block"><a href="<?=$arItem['DETAIL_PAGE_URL']?>"><?echo $arItem["NAME"]?></a></div>
+							<div class="pos-block"><?=implode(', ', array_filter([$arItem["PROPERTIES"]['POSITION']['VALUE'], $arItem["PROPERTIES"]['COMPANY']['VALUE']]))?></div>
 						</div>
-						<div class="name-block"><a href=""><?echo $arItem["NAME"]?></a></div>
-						<div class="pos-block"><?=implode(', ', array_filter([$arItem["PROPERTIES"]['POSITION']['VALUE'], $arItem["PROPERTIES"]['COMPANY']['VALUE']]))?></div>
-					</div>
-					<div class="text-block">
-					<?$str = $arItem["PREVIEW_TEXT"];
-					if (mb_strlen($str) > 150) {
-						$str = mb_substr($str, 0, 150) . '...';
-					}
-					echo $str;?>
+						<div class="text-block">
+						<?$str = $arItem["PREVIEW_TEXT"];
+						if (mb_strlen($str) > 150) {
+							$str = mb_substr($str, 0, 150) . '...';
+						}
+						echo $str;?>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	<?endforeach;?>
+		<?endforeach;?>
 	</div>
 </div>
 
