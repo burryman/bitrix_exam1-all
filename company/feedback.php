a7fd1acb9015d7f4a3304922981b0be348094026
Ilya Krasnitsky <burrmanapps@gmail.com>
2017-09-21 12:40:21 +0300

    ex1-18

---
+++ company/feedback.php
@@ -0,0 +1,15 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Обратная связь");
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:main.feedback",
+	".default",
+	Array(
+		"COMPONENT_TEMPLATE" => ".default",
+		"EMAIL_TO" => "burrmanapps@gmail.com",
+		"EVENT_MESSAGE_ID" => array(0=>"29",),
+		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
+		"REQUIRED_FIELDS" => array(),
+		"USE_CAPTCHA" => "Y"
+	)
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
